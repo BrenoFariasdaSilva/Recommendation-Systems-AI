96c80dcc712fa0ef1aee2f3d31eee8ed85e732fe
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-09-13 11:50:26 -0300

    FEAT: Adding the exceptions file for the Exceptions Class

---
+++ Module 02 - Advanced Python/02-Expections/01-exceptions.py
@@ -0,0 +1,38 @@
+# Exceptions in Python
+# Exceptions are used to handle errors and other events that may occur during the execution of a program.
+# The try statement works as follows:
+# First, the try clause (the statement(s) between the try and except keywords) is executed.
+# If no exception occurs, the except clause is skipped and execution of the try statement is finished.
+# If an exception occurs during execution of the try clause, the rest of the clause is skipped.
+# Then if its type matches the exception named after the except keyword, the except clause is executed,
+# and then execution continues after the try statement.
+
+def open_file(filename):
+	f = open(filename, "r")
+
+def open_file_with_exception(filename):
+	try: # Required
+		f = open(filename, "r")
+	except FileNotFoundError as e: # Optional
+		print(f"File {filename} ERROR: {e}")
+		return None
+	else: # Optional - Runs if no exception occurs.
+		print("Else runs if no exception occurs.")
+		return f
+	finally: # Optional - Always runs, independent if any exception occurs or not.
+		print("Finally always runs.")
+		return None
+
+def main():
+	# Open File without Exception.
+	# open_file("random.txt")
+	# print(f"")
+
+	# Open File with Exception.
+	open_file_with_exception("random.txt")
+	print(f"")
+
+	print("End of Program.")
+
+if __name__ == "__main__":
+	main()
