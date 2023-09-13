61f6d0799a11440d55383f2bafb2793fdc53c1a4
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-09-13 16:12:05 -0300

    DELETE: Removing the 01-numPy.py file in the NumPy Class

--- Module 02 - Advanced Python/05-NumPy/01-numpy.py
+++
@@ -1,5 +0,0 @@
-# NumPy -> Numerical Python.
-# NumPy is a Python library used for working with arrays.
-# It also has functions for working in domain of linear algebra, fourier transform, and matrices.
-# Libraries that use NumPy: Pandas, Matplotlib, SciPy, Scikit-learn, etc.
-
