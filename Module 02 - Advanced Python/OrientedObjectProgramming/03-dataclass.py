fc7e8dba569a15590ca9e3975693f6866b37f147
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-09-13 09:26:13 -0300

    FEAT: Adding the dataclass for the Oriented Object Programming Class

---
+++ Module 02 - Advanced Python/OrientedObjectProgramming/03-dataclass.py
@@ -0,0 +1,39 @@
+# Oriented Object Programming in Python.
+# Dataclasses are a simple way to create classes that are mainly used to store data.
+
+from dataclasses import dataclass
+
+@dataclass
+class Person:
+	name: str
+	age: int
+
+	def say_hi(self):
+		print(f"Hello, my name is {self.name} and I'm {self.age} years old.")
+
+def main():
+	me = Person("Breno", 23)
+	me.say_hi() # Hello, my name is Breno and I'm 23 years old.
+
+	me.name = "Breno Farias"
+	me.age = 24 # Update the age attribute
+	me.say_hi() # Hello, my name is Breno Farias and I'm 23 years old.
+
+	print(me) # Person(name='Breno Farias', age=24)
+	print(f"")
+
+	other = Person("Manoel Campos", 42)
+	print(other) # Person(name='Manoel Campos', age=42)
+	print(f"")
+
+	print(f"{me} == {other}: {me == other}") # False
+
+	other.name = "Breno Farias"
+	other.age = 24
+	print(other) # Person(name='Breno Farias', age=24)
+	print(f"")
+
+	print(f"{me} == {other}: {me == other}") # True
+
+if __name__ == "__main__":
+	main()
