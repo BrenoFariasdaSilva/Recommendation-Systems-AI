1080aef8f313051e9aa0ef9b9154a5a86f76fc24
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-09-13 19:00:49 -0300

    REFACTOR: Updating README.md

--- install.sh
+++ install.sh
@@ -8,8 +8,14 @@
 sudo apt-get install python3 python3-venv python3-pip -y
 # sudo apt install python-is-python3
 
-# Jupyter using Pip
+# Jupyter using Pip:
 pip install jupyterlab
 pip install notebook
 pip install ipykernel
 # Launch: jupyter-lab or jupyter notebook in terminal
+
+# Install Pip Libraries:
+pip install numpy
+pip install pandas
+pip install requests
+pip install lxml
