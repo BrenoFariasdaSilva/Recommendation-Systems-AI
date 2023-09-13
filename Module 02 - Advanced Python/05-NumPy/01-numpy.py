eb0d61b65d9237a2fcdd3633c44678b10428d033
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-09-13 16:08:33 -0300

    FEAT: Adding the 01-numPy.py file in the NumPy Class

---
+++ Module 02 - Advanced Python/05-NumPy/01-numpy.py
@@ -0,0 +1,3 @@
+# NumPy -> Numerical Python.
+# NumPy is a Python library used for working with arrays.
+# It also has functions for working in domain of linear algebra, fourier transform, and matrices.
