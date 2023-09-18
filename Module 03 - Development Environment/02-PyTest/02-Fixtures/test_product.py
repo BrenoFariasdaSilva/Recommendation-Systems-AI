4fd669bab523543c0e5be6a07c450f77bfe89fe7
Breno Farias <brenofarias@Brenos-MacBook-Pro.local>
2023-09-18 15:38:57 -0300

    REFACTOR: Updating the Fixtures python file

--- Module 03 - Development Environment/02-PyTest/02-Fixtures/test_product.py
+++ Module 03 - Development Environment/02-PyTest/02-Fixtures/test_product.py
@@ -1,3 +1,7 @@
+# Fixtures:
+# Fixtures are functions, which will run before each test function to which it is applied. Fixtures are used to feed some data to the tests such as database connections, URLs to test and some sort of input data. Therefore, instead of running the same code for every test, we can attach fixture function to the tests and it will run and return the data to the test before executing each test.
+# Advantage of Pytest Fixtures: Reusability, Reduce lines of code, Easy to maintain
+
 from product import Product
 import pytest
 
